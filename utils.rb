a934716f4693ae0bedb731d2e56a7826c459e748
Ross McKelvie <r@rossmckelvie.com>
2012-04-18 23:58:21 -0700

    traverse comments

--- utils.rb
+++ utils.rb
@@ -10,6 +10,7 @@ class Utils
     verticies = Array.new
     $i = 0
     
+    # intialize numVerts new Vertex's and push onto array
     while $i < numVerts
       v = Vertex.new($i + 1)
       verticies.push v
@@ -21,19 +22,29 @@ class Utils
   
   # Traverse a tree in DFS fashion
   def traverseTree(verticies, pos, traversal)
+    # mark vertex as touched
     verticies[pos].touch
+    
+    # get the first next vertex
     nextVert = verticies[pos].getNext
     
     while nextVert != nil
+      # get the vertex ID number
       currentVal = verticies[pos].getId
       
+      # position is always off by one, since vertex start at 1
       val = nextVert - 1
       
+      # add this one if it hasn't been touched yet and move on
       if verticies[val].touched == false
+        # add to traversal path
         traversal.add(nextVert)
+        
+        # continue traversal thru this node
         traversal = traverseTree(verticies, val, traversal)
       end
       
+      # get the next vertex deeper
       nextVert = verticies[pos].getNext
     end
     
